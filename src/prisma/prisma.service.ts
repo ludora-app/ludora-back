cad7855945da636593ff1e5946bc8d4e19ebc859
GanaF4ll <ganafall9498@gmail.com>
2025-09-22 11:05:28 +0200

    Refactor Docker configurations and update dependencies
    - Renamed environment variable files in .gitignore for clarity.
    - Enhanced Docker Compose files for development and production, including build contexts and dependencies.
    - Removed the base compose.yml file as its contents were integrated into the dev and prod configurations.
    - Updated package dependencies to their latest versions, improving stability and security.
    - Added VSCode settings for improved development experience.
    - Adjusted Prisma service initialization for better connection handling.

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,8 +1,12 @@
-import { PrismaClient } from '@prisma/client';
 import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super();
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
