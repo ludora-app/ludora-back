6e3d3daf0832a307b82d8545ef478b55a837d65a
GanaF4ll <ganafall9498@gmail.com>
2025-09-23 22:39:01 +0200

    renamed invitation resource to session-invitation

--- src/invitations/dto/input/update-invitation.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateInvitationDto } from './input/create-invitation.dto';
-
-export class UpdateInvitationDto extends PartialType(CreateInvitationDto) {}
--- src/shared/metrics/metrics.controller.ts
+++ src/shared/metrics/metrics.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { Public } from 'src/auth/decorators/public.decorator';
 import { PrometheusController } from '@willsoto/nestjs-prometheus';
+import { Public } from 'src/auth/decorators/public.decorator';
 
 @Controller('metrics')
 export class MetricsController extends PrometheusController {
