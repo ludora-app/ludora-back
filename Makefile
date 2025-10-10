91a23edaa99be1512b6490888ce40e1f051cfca4
GanaF4ll <ganafall9498@gmail.com>
2025-10-10 22:33:51 +0200

    Refactor docker-compose and Makefile: rename Grafana and Prometheus container names for consistency, and add new Makefile commands for development environment management.

--- Makefile
+++ Makefile
@@ -1,2 +1,9 @@
-nest-sh:
+dev-sh:
 		docker compose -f compose.dev.yml exec ludora-api /bin/sh
+
+dev-up:
+	docker compose -f compose.dev.yml --env-file .env.dev up --build
+
+dev-down:
+	docker compose -f compose.dev.yml --env-file .env.dev down
+
--- src/main.ts
+++ src/main.ts
@@ -16,16 +16,16 @@ async function bootstrap() {
     }),
   );
 
-  const config = new DocumentBuilder()
-    .setTitle('Ludora API v1')
-    .setDescription('API for the Ludora app')
-    .setVersion('1.0')
-    .build();
+  if (process.env.NODE_ENV !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('Ludora API v1')
+      .setDescription('API for the Ludora app')
+      .setVersion('1.0')
+      .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger', app, document);
-
-  // Note: Préférer l'injection via APP_GUARD dans un module pour une meilleure gestion des dépendances
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('swagger', app, document);
+  }
   app.useGlobalInterceptors();
   await app.listen(process.env.PORT ?? 2424);
 }
