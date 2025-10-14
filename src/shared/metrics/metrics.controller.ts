8e2884af614c5e83ece8b7d8d44caa9a7876bf53
GanaF4ll <ganafall9498@gmail.com>
2025-10-14 14:20:36 +0200

    duplicate import fixed

--- src/shared/metrics/metrics.controller.ts
+++ src/shared/metrics/metrics.controller.ts
@@ -1,7 +1,5 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { Public } from 'src/auth/decorators/public.decorator';
-import { Public } from 'src/auth/decorators/public.decorator';
-import { PrometheusController } from '@willsoto/nestjs-prometheus';
 import { PrometheusController } from '@willsoto/nestjs-prometheus';
 
 @Controller('metrics')
