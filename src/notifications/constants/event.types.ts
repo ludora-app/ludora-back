7827d09e37135f888674fe4c3ccb5eccce21d10c
GanaF4ll <ganafall9498@gmail.com>
2025-12-27 17:06:40 +0100

    better typing system in the notication & event gateway

---
+++ src/notifications/constants/event.types.ts
@@ -0,0 +1,16 @@
+/**
+ * @description Every event type that can be emitted by the application
+ */
+export enum EventTypes {
+  FRIEND_REQUEST = 'friend.request.sent',
+  FRIEND_ACCEPTED = 'friend.accepted.sent',
+  SESSION_INVITATION = 'session.invitation.sent',
+  SESSION_UPDATED = 'session.updated.sent',
+  SESSION_CANCELLED = 'session.cancelled.sent',
+  SESSION_REMINDER = 'session.reminder.sent',
+  NEW_MESSAGE = 'new.message.sent',
+  EMAIL_VERIFIED = 'email.verified',
+  NOTIFICATION_SEND = 'notification.send',
+  NOTIFICATION_BROADCAST = 'notification.broadcast',
+  NOTIFICATION_SEND_TO_MULTIPLE = 'notification.sendToMultiple',
+}
