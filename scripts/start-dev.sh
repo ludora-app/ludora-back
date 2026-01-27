44ced30210e52c17795e990964c79b3bfe4fa319
hmeberbeche <hichemamir.meberbeche@cegedim-sante.com>
2026-01-27 16:06:04 +0100

    some changes

--- scripts/start-dev.sh
+++ scripts/start-dev.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 
-# Run database migrations before starting the app
-echo "Running database migrations..."
-/usr/local/bin/vault-auth.sh envconsul -config=/etc/envconsul/config.hcl -once -- pnpm exec prisma migrate deploy
+# # Run database migrations before starting the app
+# echo "Running database migrations..."
+# /usr/local/bin/vault-auth.sh envconsul -config=/etc/envconsul/config.hcl -once -- pnpm exec prisma migrate deploy
 
 # Authentification + lancement en une seule commande (using dev Vault)
 exec /usr/local/bin/vault-auth.sh envconsul -config=/etc/envconsul/config.hcl -- node dist/src/main.js
