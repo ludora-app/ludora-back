e3ff298b466951edbc6e34306fe5b8cab4554daf
GanaF4ll <ganafall9498@gmail.com>
2026-02-10 22:19:45 +0100

    migrations

--- prisma/migrations/08_update_game_mode_preferences_unique_constraint/migration.sql
+++ prisma/migrations/08_update_game_mode_preferences_unique_constraint/migration.sql
@@ -2,6 +2,8 @@
   Warnings:
 
   - You are about to drop the column `sportsUid` on the `Fields` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[user_uid,gameMode,sport]` on the table `User_game_mode_preferences` will be added. If there are existing duplicate values, this will fail.
+
 
 */
 -- DropForeignKey
@@ -12,3 +14,7 @@ DROP INDEX "user_preferences"."User_game_mode_preferences_user_uid_gameMode_key"
 
 -- AlterTable
 ALTER TABLE "infrastructure"."Fields" DROP COLUMN "sportsUid";
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_game_mode_preferences_user_uid_gameMode_sport_key" ON "user_preferences"."User_game_mode_preferences"("user_uid", "gameMode", "sport");
+
