85795b02c35bb0d8cd7fbd94cb1fb32140625dc9
GanaF4ll <ganafall9498@gmail.com>
2026-02-11 14:09:41 +0100

    deleted useless DTOs

--- src/user-preferences/mappers/sport-preferences.mapper.ts
+++ src/user-preferences/mappers/sport-preferences.mapper.ts
@@ -1,7 +1,7 @@
 import { GameModes } from 'generated/prisma/enums';
 import { Sport } from 'src/shared/constants/constants';
 
-import { SimpleSportPreferenceResponseDto } from '../dto/output/simple-sport-preference-response.dto';
+import { SportPreferenceResponseData } from '../dto/output/sport-preference.response.dto';
 
 export interface SportPreferenceWithGameModes {
   uid: string;
@@ -14,9 +14,7 @@ export interface SportPreferenceWithGameModes {
 }
 
 export class SportPreferencesMapper {
-  static toSimpleDisplay(
-    preference: SportPreferenceWithGameModes,
-  ): SimpleSportPreferenceResponseDto {
+  static toSimpleDisplay(preference: SportPreferenceWithGameModes): SportPreferenceResponseData {
     return {
       gameModes: preference.userGameModePreferences.map((gameMode) => gameMode.gameMode),
       level: preference.level,
