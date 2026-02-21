f94c62309689bca41bd5faf5ff2a84b6ce07b950
GanaF4ll <ganafall9498@gmail.com>
2026-02-21 14:44:23 +0100

    idx_sessions_title_trgm

--- prisma/migrations/02_add_pg_trgm_indexes/migration.sql
+++ prisma/migrations/02_add_pg_trgm_indexes/migration.sql
@@ -5,3 +5,6 @@ CREATE INDEX idx_fields_name_trgm ON infrastructure."Fields" USING gin (name gin
 -- CreateIndex for pg_trgm full-text search on Users.firstname and Users.lastname
 CREATE INDEX idx_users_firstname_trgm ON auth."Users" USING gin (firstname gin_trgm_ops);
 CREATE INDEX idx_users_lastname_trgm ON auth."Users" USING gin (lastname gin_trgm_ops);
+
+-- CreateIndex for pg_trgm full-text search on Sessions.title
+CREATE INDEX idx_sessions_title_trgm ON sessions."Sessions" USING gin (title gin_trgm_ops);
