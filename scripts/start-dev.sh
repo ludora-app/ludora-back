c405cad04eb8bc46d99969a0efd97cd8af72f2d8
GanaF4ll <ganafall9498@gmail.com>
2026-02-24 21:50:43 +0100

    migrate

--- scripts/start-dev.sh
+++ scripts/start-dev.sh
@@ -3,7 +3,7 @@ set -e
 
 # # Run database migrations before starting the app
 # echo "Running database migrations..."
-# /usr/local/bin/vault-auth.sh envconsul -config=/etc/envconsul/config.hcl -once -- pnpm exec prisma migrate deploy
+/usr/local/bin/vault-auth.sh envconsul -config=/etc/envconsul/config.hcl -once -- pnpm exec prisma migrate deploy
 
 # Authentification + lancement en une seule commande (using dev Vault)
 exec /usr/local/bin/vault-auth.sh envconsul -config=/etc/envconsul/config.hcl -- node dist/src/main.js
