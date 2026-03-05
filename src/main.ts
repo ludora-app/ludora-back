7cd32099ca911776f736d65f18f71a654191d947
GanaF4ll <ganafall9498@gmail.com>
2026-03-05 09:17:37 +0100

    chore: swagger available in dev

--- src/main.ts
+++ src/main.ts
@@ -19,33 +19,33 @@ async function bootstrap() {
     }),
   );
 
-  if (process.env.NODE_ENV !== 'production') {
-    const config = new DocumentBuilder()
-      .setTitle('Ludora API')
-      .setDescription('API for the Ludora app')
-      .setVersion('0.0.1')
-      .addBearerAuth(
-        {
-          bearerFormat: 'JWT',
-          description: 'Enter JWT token',
-          in: 'header',
-          name: 'Authorization',
-          scheme: 'bearer',
-          type: 'http',
-        },
-        'JWT-auth',
-      )
-      .build();
-
-    const document = SwaggerModule.createDocument(app, config);
-
-    SwaggerModule.setup('swagger', app, document, {
-      swaggerOptions: {
-        operationsSorter: 'method',
-        tagsSorter: 'alpha',
+  // if (process.env.NODE_ENV !== 'production') {
+  const config = new DocumentBuilder()
+    .setTitle('Ludora API')
+    .setDescription('API for the Ludora app')
+    .setVersion('0.0.1')
+    .addBearerAuth(
+      {
+        bearerFormat: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+        name: 'Authorization',
+        scheme: 'bearer',
+        type: 'http',
       },
-    });
-  }
+      'JWT-auth',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('swagger', app, document, {
+    swaggerOptions: {
+      operationsSorter: 'method',
+      tagsSorter: 'alpha',
+    },
+  });
+  // }
 
   // In Docker, Fastify must listen on 0.0.0.0 to be reachable from outside the container.
   const port = Number(process.env.PORT ?? 2424);
