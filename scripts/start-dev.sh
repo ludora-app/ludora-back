df739127768ea3658ca38055bea4b9bee128378a
GanaF4ll <135319810+GanaF4ll@users.noreply.github.com>
2026-03-05 20:57:16 +0100

    Tâche scrum 446 field insertion (#153)
    * chore: updated the field data
    
    * chore: updating start scripts

--- scripts/start-dev.sh
+++ scripts/start-dev.sh
@@ -2,8 +2,8 @@
 set -e
 
 # # Run database migrations before starting the app
-# echo "Running database migrations..."
-/usr/local/bin/vault-auth.sh envconsul -config=/etc/envconsul/config.hcl -once -- pnpm exec prisma migrate deploy
+echo "Resolving any failed migrations..."
+/usr/local/bin/vault-auth.sh envconsul -config=/etc/envconsul/config.hcl -once -- pnpm exec prisma migrate resolve --rolled-back "04_alter_on_delete_user_cascades" || true
 
 # Authentification + lancement en une seule commande (using dev Vault)
 exec /usr/local/bin/vault-auth.sh envconsul -config=/etc/envconsul/config.hcl -- node dist/src/main.js
--- scripts/start-prod.sh
+++ scripts/start-prod.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 set -e
 
-# Authentification + lancement en une seule commande
+echo "Resolving any failed migrations..."
+/usr/local/bin/vault-auth.sh envconsul -config=/etc/envconsul/config.hcl -once -- pnpm exec prisma migrate resolve --rolled-back "04_alter_on_delete_user_cascades" || true
+
 echo "Running database migrations..."
 /usr/local/bin/vault-auth.sh envconsul -config=/etc/envconsul/config.hcl -once -- pnpm exec prisma migrate deploy
 
