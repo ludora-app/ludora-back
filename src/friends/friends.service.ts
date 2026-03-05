b543aede4aefb3c100f460d635db67c257c4c503
GanaF4ll <135319810+GanaF4ll@users.noreply.github.com>
2026-03-05 17:14:34 +0100

    fix: uid => friend.uid (#152)

--- src/friends/friends.service.ts
+++ src/friends/friends.service.ts
@@ -154,7 +154,7 @@ export class FriendsService {
 
     const friends = await this.prisma.$queryRaw<FriendResponseData[]>`
         SELECT 
-            uid,
+            friend.uid,
             friend.uid as "friendUid",
             f.created_at as "createdAt",
             friend.firstname,
