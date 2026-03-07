d8ece6484d46c838e6d198a5e1425d26423960c7
amir-398 <amir.398@hotmail.fr>
2026-03-07 18:52:28 +0100

    fix(SCRUM-503): revent duplicate user reports by adding a unique constraint to the schema and updating the existing report check in the service.

--- src/moderation/moderation.service.ts
+++ src/moderation/moderation.service.ts
@@ -89,6 +89,7 @@ export class ModerationService {
     const existingReport = await this.prisma.userReports.findFirst({
       where: {
         reason: report.reason,
+        reportedUid: report.reportedUid,
         reporterUid,
       },
     });
