4c01f2abcddf8c356b0f9769fc1c3b08e91f112f
GanaF4ll <ganafall9498@gmail.com>
2026-03-27 17:08:30 +0100

    fix: prod script

--- scripts/start-prod.sh
+++ scripts/start-prod.sh
@@ -5,7 +5,7 @@ set -e
 /usr/local/bin/vault-auth.sh envconsul -config=/etc/envconsul/config.hcl -once -- pnpm exec prisma migrate deploy
 
 # Seed admin accounts
-/usr/local/bin/vault-auth.sh envconsul -config=/etc/envconsul/config.hcl -- pnpm exec seed:admin
+/usr/local/bin/vault-auth.sh envconsul -config=/etc/envconsul/config.hcl -- pnpm run seed:admin:prod
 
 # Start the application
 exec /usr/local/bin/vault-auth.sh envconsul -config=/etc/envconsul/config.hcl -- node dist/src/main.js
