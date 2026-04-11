30b12fea3856fdb281b083254bfcff4f72b40d46
GanaF4ll <ganafall9498@gmail.com>
2026-04-11 10:58:34 +0200

    feat: added a magic link app store redirection endpoint based on the device

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
-
+import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiExcludeEndpoint, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { FastifyReply, FastifyRequest } from 'fastify';
 import { AppService } from './app.service';
 import { AuthB2CGuard } from './auth/guards/auth-b2c.guard';
 import { Public } from './shared/decorators/public.decorator';
@@ -14,17 +14,39 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiExcludeEndpoint()
   getHello(): string {
     return this.appService.getHello();
   }
 
   @Public()
+  @ApiExcludeEndpoint()
   @ApiOperation({ summary: 'Get the health of the application' })
   @Get('health')
-  getHealth(): { status: string; timestamp: string } {
+  getHealth(@Req() req: FastifyRequest): { status: string; timestamp: string } {
+    console.log(req.headers);
     return {
       status: 'ok',
       timestamp: new Date().toISOString(),
     };
+  }
+
+  @Public()
+  @ApiExcludeEndpoint()
+  @Get('download-app')
+  @ApiOperation({
+    summary: 'Magic link that redirects to the right app store depending on the device',
+  })
+  downloadApp(@Res() res: FastifyReply, @Req() req: FastifyRequest): void {
+    const ua = req.headers['user-agent'] || '';
+    if (/iPhone|iPad|iPod/i.test(ua)) {
+      res.redirect('https://apps.apple.com/fr/app/ludora-sport-r%C3%A9servation/id6759791424');
+    } else if (/Android/i.test(ua)) {
+      res.redirect(
+        'https://play.google.com/store/apps/details?id=com.ludora&pcampaignid=web_share',
+      );
+    } else {
+      res.redirect('https://www.ludora.fr');
+    }
   }
 }
